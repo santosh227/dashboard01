149d4e321326ff36dcfbd051a133cdca1fa28a8f
santosh227 <157505309+santosh227@users.noreply.github.com>
2024-02-07 13:50:06 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+'use strict';
+
+
+const header = document.querySelector("[data-header]");
+const navToggleBtn = document.querySelector("[data-menu-toggle-btn]");
+
+navToggleBtn.addEventListener("click", function () {
+  header.classList.toggle("active");
+});
+
+
+const menuBtn = document.querySelectorAll("[data-menu-btn]");
+
+for (let i = 0; i < menuBtn.length; i++) {
+  menuBtn[i].addEventListener("click", function () {
+    this.nextElementSibling.classList.toggle("active");
+  });
+}
+
+
+const loadMoreBtn = document.querySelector("[data-load-more]");
+
+loadMoreBtn.addEventListener("click", function () {
+  this.classList.toggle("active");
+});
